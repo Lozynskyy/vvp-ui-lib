0c1c96dd7f9b11dbda383ac003809d4cee70baa8
kalenyk1 <pasha.k.2909@gmail.com>
2018-01-15 11:13:08 +0200

    Add checkbox

--- assets/js/flipBTN.js
+++ assets/js/flipBTN.js
@@ -1,7 +1,7 @@
 
 function toggleAccordion() {
  document.querySelector('.vvp-accordion__toggle').classList.toggle('vvp-accordion__toggle--invert');
- document.querySelector('.vvp-accordion__content').classList.toggle('vvp-accordion__content--hidden');
+ document.querySelector('.vvp-accordion__content').classList.toggle('vvp-accordion__content--show');
 
 }
 let flip=document.querySelector('.vvp-accordion__header');
