45a2fb3228709a7508034ede26b04b4aa377cd08
kalenyk1 <pasha.k.2909@gmail.com>
2018-01-17 18:25:43 +0200

    Change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,6 @@ gulp.task('sass', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('sass/main.scss', ['sass']);
+    gulp.watch('sass/**/*.scss', ['sass']);
 });
+//sass --watch
