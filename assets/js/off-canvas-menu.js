2dd405c0f5bc86b5bead6ddaf6a7a4ca7becaa8e
Виталий Лозинский <vitaliylozinskiy@192-168-253-186.guest.nxc.od.ua>
2018-02-02 16:55:05 +0200

    fix menu + add styles for icons

--- assets/js/off-canvas-menu.js
+++ assets/js/off-canvas-menu.js
@@ -1,12 +1,12 @@
 let body = document.body,
 		closeButton = document.querySelector('.vvp-menu__close'),
-		openButton = document.querySelector('.vvp-menu__open'),
-		offMenuField = document.querySelector('.vvp-menu-blur');
+		openButton = document.querySelector('.vvp-menu-open'),
+		offMenuField = document.querySelector('.vvp-onmenu-background');
 
 closeButton.addEventListener('click',toggleMenu);
 openButton.addEventListener('click',toggleMenu);
 offMenuField.addEventListener('click',toggleMenu);
 
 function toggleMenu(){
-	body.classList.toggle('vvp-menu--active');
+	body.classList.toggle('vvp-menu-active');
 }
